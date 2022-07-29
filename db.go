2dcb5cfe1c6fc03444b06871d21ea116310723a2
guhkun13 <guhkun13@gmail.com>
2022-07-29 16:48:20 +0800

    update filter

--- db.go
+++ db.go
@@ -17,8 +17,19 @@ const (
 	sslmode  = "disable"
 )
 
+const (
+	limitQuery = "10"
+)
+
 type DB struct {
 	db *sql.DB
+}
+
+type QueryFilter struct {
+	Limit      string
+	KodeBiller string
+	Biller     string
+	Subbiller  string
 }
 
 func Connect() *DB {
@@ -37,35 +48,43 @@ func (d DB) Close() {
 	defer d.db.Close()
 }
 
+func buildFilter(r *http.Request) QueryFilter {
+	var filter QueryFilter
+	limit := r.URL.Query().Get("limit")
+	kodeBiller := r.URL.Query().Get("biller")
+
+	if limit == "" {
+		limit = limitQuery
+	}
+
+	if len(kodeBiller) == 8 {
+		filter.Biller = kodeBiller[:4]
+		filter.Subbiller = kodeBiller[4:]
+	}
+	filter.Limit = limit
+
+	return filter
+}
+
+func sqlFilterInquiry(r *http.Request, filter QueryFilter) string {
+	sql := "select ts, ts_resp, rc, biller, subbiller, id_byr, nama from inquiry"
+
+	sql += " where biller = '" + filter.Biller + "'"
+	if filter.Subbiller != "" {
+		sql += " and subbiller = '" + filter.Subbiller + "'"
+	}
+	sql += " order by ts desc limit " + filter.Limit
+	fmt.Println(sql)
+
+	return sql
+}
+
 func (d DB) GetLatestInquiryRecords(r *http.Request) ([]Inquiry, error) {
 	var inquirys []Inquiry
 
-	var (
-		limit      string
-		biller     string
-		subbiller  string
-		kodeBiller string
-	)
+	filter := buildFilter(r)
+	sql := sqlFilterInquiry(r, filter)
 
-	limit = r.URL.Query().Get("limit")
-	kodeBiller = r.URL.Query().Get("biller")
-
-	if limit == "" {
-		limit = "10"
-	}
-	sql := "select ts, ts_resp, rc, biller, subbiller, id_byr, nama from inquiry "
-	if len(kodeBiller) == 8 {
-		biller = kodeBiller[:4]
-		subbiller = kodeBiller[4:]
-	}
-	sql += " where biller = '" + biller + "'"
-	if subbiller != "" {
-		sql += " and subbiller = '" + subbiller + "'"
-
-	}
-	sql += " order by ts desc limit " + limit
-
-	fmt.Println(sql)
 	rows, err := d.db.Query(sql)
 	if err != nil {
 		return nil, err
